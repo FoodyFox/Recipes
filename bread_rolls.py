896dcdc907dfff39cd04424909b6156af9a04ad9
FoodyFox <148678935+FoodyFox@users.noreply.github.com>
2025-09-28 17:12:05 +0100

    Update bread_rolls.py

--- bread_rolls.py
+++ bread_rolls.py
@@ -27,23 +27,25 @@ Ingredients:
 
 Method:
 
-    Add the water, butter and sugar to a saucepan and heat until melted.
+    Add the water, butter and sugar to a saucepan and heat until the butter is melted.
 
     Add cold milk to the saucepan and check the resulting temperature is 30 to 35C and add the yeast
 
     Add the remaining dry ingredients (except the egg) to a stand mixer with a dough hook and mix on low whilst slowly adding the wet ingredients
 
-    Add the egg and mix on medium for 3 minutes, scrape the bowl down and rest for 3 minutes.repeat the mixing and resting another two times
+    Add the egg and mix on medium for 3 minutes, scrape the bowl down and rest for 3 minutes. Repeat the mixing and resting another two times
 
     Cover the dough and allow to rise for 30 to 60 mins
 
     Split the dough in to 12 for small burger buns, 8 for large burger buns or 4 for very large subs
 
-    leave to rise in a warm place for 30-40 minutes covered with a tea towel
+    Shape as needed and place on baking parchment.
 
-    brush with milk and optionally top with sesame seeds
+    Leave to rise in a warm place for 30-40 minutes covered with a tea towel
 
-    Bake at 200C for 10-12 minutes until golden brown
+    Brush with milk and optionally top with sesame seeds
+
+    Bake on a pizza stone at 200C for 12-15 minutes until golden brown. Rotate after 10 minutes if needed.
 
     Brush with butter as soon as they come out the oven for a lovely sheen
 
